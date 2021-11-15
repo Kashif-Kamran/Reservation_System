317f2d0b6256783392b9a701226380da684cd694
Kashif-Kamran <i190541@nu.edu.pk>
2021-11-16 04:20:50 +0500

    Test Cases of Reservation Class

--- src/test/java/TestCases/ReservationTest.java
+++ src/test/java/TestCases/ReservationTest.java
@@ -9,17 +9,54 @@ import Code.Reservation;
 
 public class ReservationTest
 {
-
+	final int fstSeats = 10;
+	final int busSeats = 20;
+	final int ecoSeats = 30;
 	Reservation reserve;
+
+	public boolean ReserveAllSeats()
+	{
+		for (int i = 1; i <= fstSeats; i++)
+		{
+			try
+			{
+				reserve.reserveSeat("FST", i);
+			} catch (Exception exc)
+			{
+				return false;
+			}
+		}
+		for (int i = 1; i <= busSeats; i++)
+		{
+			try
+			{
+				reserve.reserveSeat("BUS", i);
+			} catch (Exception exc)
+			{
+				return false;
+			}
+		}
+		for (int i = 1; i <= ecoSeats; i++)
+		{
+			try
+			{
+				reserve.reserveSeat("ECO", i);
+			} catch (Exception exc)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
 
 	@Before
 	public void BeforeTestCases()
 	{
-		reserve = new Reservation(10, 20, 30);
+		reserve = new Reservation(fstSeats, busSeats, ecoSeats);
 	}
 
 	@Test
-	public void SeatReservation_Test1()
+	public void Test1_SeatReservation()
 	{
 		boolean check;
 		for (int i = 1; i <= 10; i++)
@@ -34,9 +71,47 @@ public class ReservationTest
 		Assert.assertTrue(true);
 	}
 
+	@Test
+	public void Test2_ReserveAllSeat_Pos()
+	{
+		this.ReserveAllSeats();
+	}
+
+	@Test
+	public void Test3_ReserveEndSeat()
+	{
+		// Initial Seat
+		Assert.assertTrue(reserve.reserveSeat("FST", 1));
+		Assert.assertTrue(reserve.reserveSeat("ECO", 1));
+		Assert.assertTrue(reserve.reserveSeat("BUS", 1));
+		// Last Seats
+		Assert.assertTrue(reserve.reserveSeat("FST", fstSeats));
+		Assert.assertTrue(reserve.reserveSeat("ECO", ecoSeats));
+		Assert.assertTrue(reserve.reserveSeat("BUS", busSeats));
+	}
+
+	@Test
+	public void Test4_UnReserveSeat()
+	{
+		this.ReserveAllSeats();
+		Assert.assertTrue(reserve.unReserveSeat("FST", 1));
+		Assert.assertTrue(reserve.unReserveSeat("FST", 5));
+		Assert.assertTrue(reserve.unReserveSeat("FST", 7));
+	}
+
+	@Test
+	public void Test5_ShowDisplay()
+	{
+		reserve.showAllSeats();
+		reserve.showAvailableSeats();
+		reserve.showReservedSeats();
+		Assert.assertTrue(true);
+	}
+
 	@After
 	public void AfterTestCases()
 	{
 		reserve = null;
+		// Memory Collector
 	}
 }
